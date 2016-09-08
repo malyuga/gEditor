c93c674a368ae589f00662545761091cb219150e
malyuga <foma.tapo4kin@yandex.ru>
2016-09-09 02:29:43 +0300

    added sql scripts to initialize the DB

---
+++ SQL/initDB.sql
@@ -0,0 +1,7 @@
+start "D:/gitRepositories/gEditor/SQL/createGraphIDSeq";
+start "D:/gitRepositories/gEditor/SQL/createGraphTable";
+start "D:/gitRepositories/gEditor/SQL/createNodeIDSeq";
+start "D:/gitRepositories/gEditor/SQL/createNodeTable";
+start "D:/gitRepositories/gEditor/SQL/createEdgeTable";
+start "D:/gitRepositories/gEditor/SQL/createNewGraphProc";
+start "D:/gitRepositories/gEditor/SQL/createNewNodeProc";
